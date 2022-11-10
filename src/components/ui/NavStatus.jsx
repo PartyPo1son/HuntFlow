68c1c1d50aa451154c74d66310b2305bd4c77e82
Ko.Kko <ko.kko@mrm.ru>
2022-11-10 16:11:34 +0300

    Style

--- src/components/ui/NavStatus.jsx
+++ src/components/ui/NavStatus.jsx
@@ -5,8 +5,8 @@ export default function NavStatus({ allStatus, statusHandler }) {
   return (
     <ul className="nav nav-tabs">
       {allStatus.map((el) => (
-        <li className="nav-item">
-          <button key={el.id} type="button" onClick={() => statusHandler(el.id)} className="nav-link">
+        <li className="nav-item ">
+          <button key={el.id} type="button" onClick={() => statusHandler(el.id)} className="nav-link ">
             {el.title}
             {' '}
           </button>
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -2,33 +2,26 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#e3f4fc' }}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">HuntFlow</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="/addCard">Добавить кандидата</a>
+            </li>
+
+          </ul>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link" href="/reg">Регистрация</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/auth">Авторизация</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Выпадающий список
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a className="dropdown-item" href="#">Действие</a></li>
-                <li><a className="dropdown-item" href="#">Другое действие</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Что-то еще здесь</a></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Отключенная</a>
             </li>
           </ul>
 

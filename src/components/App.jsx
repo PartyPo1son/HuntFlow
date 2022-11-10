2dc47495739fcf1177e8a1267939d6bbda980718
Kirill Prikhozhij <mentblead@gmail.com>
2022-11-10 15:19:14 +0300

    merge1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,9 @@ import Reg from './Reg';
 import Auth from './Auth';
 import CandidatCards from './CandidatCards';
 
-export default function App({ candidat }{ allStatus, allCandidates, vacansy }) {
+export default function App({
+  candidat, allStatus, allCandidates, vacansy,
+}) {
 // РАБОТА С ДОБАВЛЕНИЕМ КАНДИДАТА
   const [groupInput, setGroupInput] = useState({
     first_name: '',
@@ -50,7 +52,7 @@ export default function App({ candidat }{ allStatus, allCandidates, vacansy }) {
         <Route path="/" element={<MainPage vacansy={vacansy} allStatus={allStatus} allCandidates={allCandidates} />} />
         <Route path="/addCard" element={<AddCardPage groupInput={groupInput} inputHandler={inputHandler} submitHandler={submitHandler} />} />
         <Route path="/addCard/:id" element={<ThanksPage candidat={candidat} />} />
-        <Route path="/candidat/:id" element={<CandidatCards candidat={candidat}/>} />
+        <Route path="/candidat/:id" element={<CandidatCards candidat={candidat} />} />
         <Route path="/reg" element={<Reg />} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
